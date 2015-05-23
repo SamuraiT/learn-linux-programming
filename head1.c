5a9e2e44012eea6f459b3272f976cf8e5e3d084b
SamuraiT <t.yasukawa01@gmail.com>
2015-05-23 21:17:14 +0900

    update usage

--- head1.c
+++ head1.c
@@ -6,7 +6,7 @@ static void head_files(char *path, long nlines);
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
+    fprintf(stderr, "Usage: %s n [file file ....]\n", argv[0]);
     exit(1);
   }
   long nlines = atol(argv[1]);

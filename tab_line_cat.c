d4b4e32de238774aec865359d7936abe0ab8e1b8
SamuraiT <t.yasukawa01@gmail.com>
2015-05-23 23:03:03 +0900

    add command option

--- tab_line_cat.c
+++ tab_line_cat.c
@@ -4,22 +4,48 @@
  * cat with stdio
  *
  */
-#define TAB 9
-#define LINE_BREAK 10
+#define TAB '\t'
+#define LINE_BREAK '\n'
+typedef int bool;
+#define true 1
+#define false 0
+#define _GNU_SOURCE
+#include <getopt.h>
 
 void print_escaped_tab();
 void print_escaped_line_break();
 
+static struct option longopts[] = {
+  {"literal", no_argument, NULL, 'l'},
+  {0, 0, 0, 0}
+};
+
 int main(int argc, char *argv[]) {
+  int opt;
+  bool should_show_literal = false;
+
+  while((opt = getopt_long(argc, argv, "l", longopts, NULL)) != -1){
+    switch (opt) {
+      case 'l':
+        should_show_literal = true;
+        break;
+      case '?':
+        fprintf(stderr, "Usage: %s [l]\n", argv[0]);
+        exit(1);
+    }
+  }
+
   int i;
-  for (i=1; i < argc; i++){
+  for (i=optind; i < argc; i++){
     FILE *f;
     int c;
     f = fopen(argv[i], "r");
     if (!f) { perror(argv[i]); exit(1); }
     while ( (c = fgetc(f)) != EOF) {
-      if ( c == TAB ) { print_escaped_tab(); continue; }
-      if ( c == LINE_BREAK ) { print_escaped_line_break(); continue; }
+      if (should_show_literal) {
+        if ( c == TAB ) { print_escaped_tab(); continue; }
+        if ( c == LINE_BREAK ) { print_escaped_line_break(); continue; }
+      }
       if (putchar(c) < 0) exit(1);
     }
     fclose(f);
@@ -38,7 +64,7 @@ void print_escaped_line_break () {
   int dollar = 32;
   int slash = 92;
   int n = 110;
-  if (putchar(32) < 0) exit(1);
-  if (putchar(92) < 0) exit(1);
-  if (putchar(110) < 0) exit(1);
+  if (putchar(dollar) < 0) exit(1);
+  if (putchar(slash) < 0) exit(1);
+  if (putchar(n) < 0) exit(1);
 }

5b069c42dbd52036ed08c2f649bac6d9d4f9b8fc
SamuraiT <t.yasukawa01@gmail.com>
2015-05-24 13:05:56 +0900

    Add inverse grep

--- grep.c
+++ grep.c
@@ -3,22 +3,48 @@
 #include <regex.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
+static struct option longopts[] = {
+  {"ignore-case", required_argument, NULL, 'i'},
+  {"invert-match", required_argument, NULL, 'v'},
+  {0,0,0,0}
+};
+
+typedef void (* GREP_POINTER)(regex_t *pattern, FILE *src);
 static void grep(regex_t *pattern, FILE *src);
-static void grep_files(regex_t *pattern, char *path);
+static void grep_files(regex_t *pattern, char *path, GREP_POINTER _grep);
 static void reg_error(int err, regex_t *pattern);
+static void invert_grep(regex_t *pattern, FILE *src);
 
 int main(int argc, char *argv[]) {
   regex_t pattern;
-  int err;
-  int i;
+  char *pat = '\0';
+  int err, opt, i;
+  GREP_POINTER _grep = grep;
+  while ((opt = getopt_long(argc, argv, "i:v:", longopts, NULL)) != -1) {
+    switch (opt) {
+      case 'i':
+        pat = optarg;
+        break;
+      case 'v':
+        pat = optarg;
+        _grep = invert_grep;
+        break;
+      case '?':
+        fprintf(stdout, "Usage: %s [-i, -v] [FILE ...]\n", argv[0]);
+        exit(1);
+    }
+  }
+  if (!pat) pat = argv[optind];
   if (argc < 2) { fputs("no pattern\n", stderr); exit(1); }
-
-  err = regcomp(&pattern, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+  err = regcomp(&pattern, pat, REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
   if (err != 0) reg_error(err, &pattern);
-  if (argc == 2) { grep(&pattern, stdin); }
+  if (argc == optind) { _grep(&pattern, stdin);}
   else {
-    for (i=2; i < argc; i++)
-      grep_files(&pattern, argv[i]);
+    for (i=optind; i < argc; i++)
+      grep_files(&pattern, argv[i], _grep);
   }
   regfree(&pattern);
   exit(0);
@@ -39,11 +65,20 @@ static void grep(regex_t *pattern, FILE *src) {
   }
 }
 
-static void grep_files(regex_t *pattern, char *path) {
+static void invert_grep(regex_t *pattern, FILE *src) {
+  char buf[4096];
+  while (fgets(buf, sizeof buf, src)) {
+    if (regexec(pattern, buf, 0, NULL, 0) != 0)
+      fputs(buf, stdout);
+  }
+}
+
+
+static void grep_files(regex_t *pattern, char *path, GREP_POINTER _grep) {
   FILE *f;
   f = fopen(path, "r");
   if(!f) { perror(path); exit(1); }
-  grep(pattern, f);
+  _grep(pattern, f);
   fclose(f);
 }
 
